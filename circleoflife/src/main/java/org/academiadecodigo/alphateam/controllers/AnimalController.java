88154c3613fe03113d028b349dfa715d491caf28
Sara Oliveira <pointspotdot@gmail.com>
2019-04-17 22:08:46 +0100

    updated animal controller

---
+++ circleoflife/src/main/java/org/academiadecodigo/alphateam/controllers/AnimalController.java
@@ -0,0 +1,17 @@
+package org.academiadecodigo.alphateam.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pet")
+public class AnimalController {
+
+    private UserService userService;
+    private AnimalService animalService;
+    private AnimalToAnimalDTO animalToAnimalDto;
+    private AnimalDtoToAnimal animalDtoToAnimal;
+
+    
+
+}
