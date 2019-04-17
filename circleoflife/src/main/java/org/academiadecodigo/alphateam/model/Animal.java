d3fbad0df75c065442602d7b5cb4436e55ca9b55
diefersonmatos <diefersonamatos@gmail.com>
2019-04-17 23:54:14 +0100

    Update Animal Second Try

--- circleoflife/src/main/java/org/academiadecodigo/alphateam/model/Animal.java
+++ circleoflife/src/main/java/org/academiadecodigo/alphateam/model/Animal.java
@@ -1,4 +1,68 @@
 package org.academiadecodigo.alphateam.model;
 
 public class Animal {
+
+    private String name;
+    private String birthdate;
+    private String size;
+    private String color;
+    private String breed;
+    private Boolean neutered;
+    private double weigth;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Boolean getNeutered() {
+        return neutered;
+    }
+
+    public void setNeutered(Boolean neutered) {
+        this.neutered = neutered;
+    }
+
+    public double getWeigth() {
+        return weigth;
+    }
+
+    public void setWeigth(double weigth) {
+        this.weigth = weigth;
+    }
 }
