1992d899419edc349f6808b1e51600fdfa1c7796
diefersonmatos <diefersonamatos@gmail.com>
2019-04-17 21:29:03 +0100

    Update genericdao

--- circleoflife/src/main/java/org/academiadecodigo/alphateam/dao/jpa/GenericJpaDao.java
+++ circleoflife/src/main/java/org/academiadecodigo/alphateam/dao/jpa/GenericJpaDao.java
@@ -9,7 +9,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
-public class GenericJpaDao <T extends Model> implements Dao<T> {
+public abstract class GenericJpaDao <T extends Model> implements Dao<T> {
 
     protected Class<T> modelType;
 
