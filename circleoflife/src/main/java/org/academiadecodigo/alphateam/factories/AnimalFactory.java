ec5e93ddb24cbedbe1e960c5c329e2be5e783a7a
diefersonmatos <diefersonamatos@gmail.com>
2019-04-17 19:34:51 +0100

    update model
    Co-Authored-By: brunotx <brunotx@users.noreply.github.com>

---
+++ circleoflife/src/main/java/org/academiadecodigo/alphateam/factories/AnimalFactory.java
@@ -0,0 +1,31 @@
+package org.academiadecodigo.alphateam.factories;
+
+import org.academiadecodigo.alphateam.model.animal.Animal;
+import org.academiadecodigo.alphateam.model.animal.AnimalType;
+import org.academiadecodigo.alphateam.model.animal.Cat;
+import org.academiadecodigo.alphateam.model.animal.Dog;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalFactory {
+
+    public Animal createAccount(AnimalType animalType) {
+
+        Animal newAnimal;
+
+        switch (animalType) {
+            case CAT:
+                newAnimal = new Cat();
+                break;
+            case DOG:
+                newAnimal = new Dog();
+                break;
+            default:
+                throw new IllegalArgumentException("Animal invalid");
+        }
+
+        return newAnimal;
+    }
+
+
+}
---
+++ circleoflife/src/main/java/org/academiadecodigo/alphateam/model/Model.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.alphateam.model;
+
+public interface Model {
+
+    Integer getId();
+
+    void setId(Integer id);
+
+}
