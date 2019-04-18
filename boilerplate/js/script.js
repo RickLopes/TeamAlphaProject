039d45caf70a74fe4d762ae3003509c73fb6331f
RickLopes <rick.lopes.96@gmail.com>
2019-04-18 03:00:37 +0100

    add logo updated index

--- boilerplate/js/script.js
+++ boilerplate/js/script.js
@@ -2,6 +2,16 @@ var API_URL_CUSTOMER = "http://localhost:8080/javabank5/api/customer";
 var API_URL_CUSTOMER = "http://localhost:8080/javabank5/api/customer/";
 var API_URL_CUSTOMER = "http://localhost:8080/javabank5/api/customer";
 
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
+
 // add an animal
 function addAnimal() {
   $.ajax({
@@ -26,10 +36,10 @@ var populateWithAnimals = function(json) {
   json.forEach(function(element) {
     var htmlString =
       '<div class="col-md-4 mb-5">' +
-      ' <div class="card h-100"> ' +
+      ' <div class="card h-100" id="card-'+ element.id +'>' +
       '   <img class="card-img-top" src="http://placehold.it/300x200" alt="">' +
       '   <div class="card-body">' +
-      '     <h4 class="card-title">'+ element.+'</h4>' +
+      '     <h4 class="card-title">'+ element.name+'</h4>' +
       '     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>' +
       '   </div>' +
       '   <div class="card-footer">' +
